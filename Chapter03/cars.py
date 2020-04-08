a41a004a8a08ae793a120e8437b1d653ad26cf3b
PhlipTung <philip.dongfei@gmail.com>
2020-04-08 13:32:20 +0800

    Chapter 03 Introducing Lists

---
+++ Chapter03/cars.py
@@ -0,0 +1,17 @@
+cars = ['bmw','audi','toyota','subaru']
+
+print(cars)
+print(sorted(cars))
+print(cars)
+print(sorted(cars, reverse=True))
+print(cars)
+cars.reverse()
+print(cars)
+
+
+
+cars.sort()
+print(cars)
+cars.sort(reverse=True)
+print(cars)
+

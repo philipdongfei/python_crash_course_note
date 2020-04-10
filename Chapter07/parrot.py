3aa992e9a792c023d8226aab560a5ee7aa687efd
PhlipTung <philip.dongfei@gmail.com>
2020-04-10 14:05:54 +0800

    Chapter 07 User Input and while Loops

---
+++ Chapter07/parrot.py
@@ -0,0 +1,24 @@
+message = input("Tell me something, and I will repeat it back to you: ")
+print(message)
+
+prompt = "\nTell me something, and I will repeat it back to you:"
+prompt += "\nEnter 'quit' to end the program."
+
+'''
+
+message = ""
+while message != 'quit':
+    message = input(prompt)
+    if message != 'quit':
+        print(message)
+'''
+activate = True
+while activate:
+    message = input(prompt)
+    if message == 'quit':
+        activate = False
+    else:
+        print(message)
+
+
+

542f3b4dc6a2b1595a48ae96d60fe9c99f6742b4
PhlipTung <philip.dongfei@gmail.com>
2020-04-26 15:10:13 +0800

    Chapter 19 User Accounts

--- Chapter18/learning_log/settings.py
+++ Chapter18/learning_log/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # My apps
     'learning_logs',
+    'users',
+
 ]
 
 MIDDLEWARE = [
@@ -120,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# My settings
+LOGIN_URL = 'users:login'

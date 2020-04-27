587339b5b9c2acf9a511c2c0958b5fe3c7406cd1
PhlipTung <philip.dongfei@gmail.com>
2020-04-27 11:15:21 +0800

    Chapter 20 Styling and Deploying an App, styling learning log

---
+++ Chapter20/ll_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/philip/LearnDemo/Python_Crash_Course/Chapter17/ll_env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
